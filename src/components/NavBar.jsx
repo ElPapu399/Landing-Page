90554731e8f03179d937d63b81816306e4f51b7f
Dylan <dylan@gmail.com>
2026-04-17 00:08:49 -0500

    Correción nueva reciente

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,7 +13,7 @@ function NavBar() {
                     <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                         <li><a href="#" className="nav-link px-2 text-secondary">Inicio</a></li>
                         <li><a href="#" className="nav-link px-2 text-white">Sobre mi</a></li>
-                        <li><a href="#" className="nav-link px-2 text-white">Proyectos</a></li>
+                        <li><a href="#" className="nav-link px-2 text-white">Mis proyectos</a></li>
                         <li><a href="#" className="nav-link px-2 text-white">Contacto</a></li>
                     </ul>
                     <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
